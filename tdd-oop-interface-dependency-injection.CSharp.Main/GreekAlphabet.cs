1884b20682fb377ae7bbe2045e7922d550c89aac
John Ferdie Abueg <john.abueg@gmail.com>
2024-08-21 11:09:17 +0200

    Refactored code so that it loops an alphabet instead

--- tdd-oop-interface-dependency-injection.CSharp.Main/GreekAlphabet.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/GreekAlphabet.cs
@@ -12,47 +12,83 @@ namespace tdd_oop_interface_dependency_injection.CSharp.Main
         {
             Dictionary<Char, int> words = new Dictionary<Char, int>();
 
-            string onePoint = "αειοφξ";
-            foreach (char c in onePoint.ToCharArray())
+
+
+            string greekAlphabet = "αειοφξβδπγηντζκρψλθχμσυω";
+
+            foreach (char c in greekAlphabet.ToCharArray())
             {
-                words.Add((char)c, 1);
+                if (c == 'α' || c == 'ε' || c == 'ι' || c == 'ο' || c == 'φ' || c == 'ξ')
+                {
+                    words.Add(c, 1);
+                }
+                else if (c == 'β' || c == 'δ' || c == 'π')
+                {
+                    words.Add(c, 2);
+                }
+                else if (c == 'γ' || c == 'η' || c == 'ν' || c == 'τ')
+                {
+                    words.Add(c, 3);
+                }
+                else if (c == 'ζ' || c == 'κ' || c == 'ρ' || c == 'ψ' || c == 'λ')
+                {
+                    words.Add(c, 4);
+                }
+                else if (c == 'θ' || c == 'χ')
+                {
+                    words.Add(c, 5);
+                }
+                else if (c == 'μ' || c == 'σ')
+                {
+                    words.Add(c, 8);
+                }
+                else if (c == 'υ' || c == 'ω')
+                {
+                    words.Add(c, 10);
+                }
             }
 
-            string twoPoints = "βδπ";
-            foreach (char c in twoPoints.ToCharArray())
-            {
-                words.Add((char)c, 2);
-            }
+            //string onePoint = "αειοφξ";
+            //foreach (char c in onePoint.ToCharArray())
+            //{
+            //    words.Add((char)c, 1);
+            //}
 
-            string threePoints = "γηντ";
-            foreach (char c in threePoints.ToCharArray())
-            {
-                words.Add((char)c, 3);
-            }
+            //string twoPoints = "βδπ";
+            //foreach (char c in twoPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 2);
+            //}
 
-            string fourPoints = "ζκρψλ";
-            foreach (char c in fourPoints.ToCharArray())
-            {
-                words.Add((char)c, 4);
-            }
+            //string threePoints = "γηντ";
+            //foreach (char c in threePoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 3);
+            //}
 
-            string fivePoints = "θχ";
-            foreach (char c in fivePoints.ToCharArray())
-            {
-                words.Add((char)c, 5);
-            }
+            //string fourPoints = "ζκρψλ";
+            //foreach (char c in fourPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 4);
+            //}
 
-            string eightPoints = "μσ";
-            foreach (char c in eightPoints.ToCharArray())
-            {
-                words.Add((char)c, 8);
-            }
+            //string fivePoints = "θχ";
+            //foreach (char c in fivePoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 5);
+            //}
 
-            string tenPoints = "υω";
-            foreach (char c in tenPoints.ToCharArray())
-            {
-                words.Add((char)c, 10);
-            }
+            //string eightPoints = "μσ";
+            //foreach (char c in eightPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 8);
+            //}
+
+            //string tenPoints = "υω";
+            //foreach (char c in tenPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 10);
+            //}
 
             return words;
         }
--- tdd-oop-interface-dependency-injection.CSharp.Main/RussianAlphabet.cs
+++ tdd-oop-interface-dependency-injection.CSharp.Main/RussianAlphabet.cs
@@ -12,46 +12,74 @@ namespace tdd_oop_interface_dependency_injection.CSharp.Main
         {
             Dictionary<Char, int> words = new Dictionary<Char, int>();
 
-            string onePoint = "абвгд";
-            foreach (char c in onePoint.ToCharArray())
+            string russianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэю";
+
+            foreach (char c in russianAlphabet.ToCharArray())
             {
-                words.Add((char)c, 1);
+                if (c == 'a' || c == 'б' || c == 'в' || c == 'г' || c == 'д')
+                {
+                    words.Add(c, 1);
+                } else if (c == 'е' || c == 'ё' || c == 'ж' || c == 'з' || c == 'и')
+                {
+                    words.Add(c, 2);
+                } else if (c == 'й' || c == 'к' || c == 'л' || c == 'м' || c == 'н')
+                {
+                    words.Add(c, 3);
+                } else if (c == 'о' || c == 'п' || c == 'р' || c == 'с' || c == 'т' || c == 'у' || c == 'ф')
+                {
+                    words.Add(c, 4);
+                } else if (c == 'х' || c == 'ц' || c == 'ч' || c == 'ш')
+                {
+                    words.Add(c, 5);
+                } else if (c == 'щ' || c == 'ъ' || c == 'ы')
+                {
+                    words.Add(c, 8);
+                } else if (c == 'ь' || c == 'э' || c == 'ю')
+                {
+                    words.Add(c, 10);
+                }
             }
 
-            string twoPoints = "еёжзи";
-            foreach (char c in twoPoints.ToCharArray())
-            {
-                words.Add((char)c, 2);
-            }
+            //string onePoint = "абвгд";
+            //foreach (char c in onePoint.ToCharArray())
+            //{
+            //    words.Add((char)c, 1);
+            //}
 
-            string threePoints = "йклмн";
-            foreach (char c in threePoints.ToCharArray()) { 
-                words.Add((char)c, 3);
-            }
+            //string twoPoints = "еёжзи";
+            //foreach (char c in twoPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 2);
+            //}
 
-            string fourPoints = "опрстуф";
-            foreach (char c in fourPoints.ToCharArray())
-            {
-                words.Add((char)c, 4);
-            }
+            //string threePoints = "йклмн";
+            //foreach (char c in threePoints.ToCharArray()) { 
+            //    words.Add((char)c, 3);
+            //}
 
-            string fivePoints = "хцчш";
-            foreach (char c in fivePoints.ToCharArray())
-            {
-                words.Add((char)c, 5);
-            }
+            //string fourPoints = "опрстуф";
+            //foreach (char c in fourPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 4);
+            //}
 
-            string eightPoints = "щъы";
-            foreach (char c in eightPoints.ToCharArray())
-            {
-                words.Add((char)c, 8);
-            }
+            //string fivePoints = "хцчш";
+            //foreach (char c in fivePoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 5);
+            //}
 
-            string tenPoints = "ьэю";
-            foreach (char c in tenPoints.ToCharArray())
-            {
-                words.Add((char)c, 10);
-            }
+            //string eightPoints = "щъы";
+            //foreach (char c in eightPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 8);
+            //}
+
+            //string tenPoints = "ьэю";
+            //foreach (char c in tenPoints.ToCharArray())
+            //{
+            //    words.Add((char)c, 10);
+            //}
 
             return words;
         }
